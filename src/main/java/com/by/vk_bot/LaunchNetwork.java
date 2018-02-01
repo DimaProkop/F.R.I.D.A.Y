b732b09942ec4dd8025237b911b157d39fb93c98
prokop <dionp95@mail.ru>
2018-02-01 17:59:40 +0300

    success start check new data and current rules for network

--- src/main/java/com/by/vk_bot/LaunchNetwork.java
+++ src/main/java/com/by/vk_bot/LaunchNetwork.java
@@ -7,11 +7,43 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class LaunchNetwork {
+
+    private static final String trainDataFile = "/Users/prokop06/IdeaProjects/F.R.I.D.A.Y/src/main/java/com/by/vk_bot/train.data";
+
     public static void main(String[] args) {
-        trainingNetwork();
+
+        trainingNetwork(trainDataFile);
+
+        Fann fann = new Fann("ann");
+        float[][] tests = {
+                {1.0f, 0, 1},
+                {0.9f, 1, 3},
+                {0.3f, 0, 8},
+                {1, 1, 8},
+                {0.1f, 0, 0},
+        };
+        for (float[] test:tests){
+            System.out.println(getAction(fann.run(test)));
+        }
     }
 
-    private static void trainingNetwork() {
+    private static String getAction(float[] out){
+        int i = 0;
+        for (int j = 1; j < 4; j++) {
+            if(out[i]<out[j]){
+                i = j;
+            }
+        }
+        switch (i){
+            case 0:return "атаковать";
+            case 1:return "прятаться";
+            case 2:return "бежать";
+            case 3:return "ничего не делать";
+        }
+        return "";
+    }
+
+    private static void trainingNetwork(String path) {
         List<Layer> layerList = new ArrayList<>();
         layerList.add(Layer.create(3, ActivationFunction.FANN_SIGMOID_SYMMETRIC, 0.01f));
 
@@ -25,7 +57,7 @@ public class LaunchNetwork {
         /* Проведем обучение взяв уроки из файла, с максимальным колличеством
            циклов 100000, показывая отчет каждую 100ю итерацию и добиваемся
         ошибки меньше 0.0001 */
-        trainer.train(new File("train.data").getAbsolutePath(), 100000, 100, 0.0001f);
+        trainer.train(path, 100000, 100, 0.0001f);
         fann.save("ann");
     }
 }
