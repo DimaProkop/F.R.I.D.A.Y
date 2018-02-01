cb43b69d22a105043f06dce97dd54605c693bd23
prokop <dionp95@mail.ru>
2018-02-01 17:34:48 +0300

    delete gradle, add network from FANN lib (train.data in root folder)

---
+++ src/main/java/com/by/vk_bot/LaunchNetwork.java
@@ -0,0 +1,31 @@
+package com.by.vk_bot;
+
+import com.googlecode.fannj.*;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+public class LaunchNetwork {
+    public static void main(String[] args) {
+        trainingNetwork();
+    }
+
+    private static void trainingNetwork() {
+        List<Layer> layerList = new ArrayList<>();
+        layerList.add(Layer.create(3, ActivationFunction.FANN_SIGMOID_SYMMETRIC, 0.01f));
+
+        layerList.add(Layer.create(16, ActivationFunction.FANN_SIGMOID_SYMMETRIC, 0.01f));
+        layerList.add(Layer.create(4, ActivationFunction.FANN_SIGMOID_SYMMETRIC, 0.01f));
+        Fann fann = new Fann(layerList);
+        //Создаем тренера и определяем алгоритм обучения
+        Trainer trainer = new Trainer(fann);
+        trainer.setTrainingAlgorithm(TrainingAlgorithm.FANN_TRAIN_RPROP);
+
+        /* Проведем обучение взяв уроки из файла, с максимальным колличеством
+           циклов 100000, показывая отчет каждую 100ю итерацию и добиваемся
+        ошибки меньше 0.0001 */
+        trainer.train(new File("train.data").getAbsolutePath(), 100000, 100, 0.0001f);
+        fann.save("ann");
+    }
+}
