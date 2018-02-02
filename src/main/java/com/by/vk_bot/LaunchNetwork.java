bea5a72dcb872f15184ca348224ad9ca42ce4301
prokop <dionp95@mail.ru>
2018-02-02 10:12:08 +0300

    fix path or data files

--- src/main/java/com/by/vk_bot/LaunchNetwork.java
+++ src/main/java/com/by/vk_bot/LaunchNetwork.java
@@ -14,12 +14,12 @@ public class LaunchNetwork {
 
         trainingNetwork(trainDataFile);
 
-        Fann fann = new Fann("ann");
+        Fann fann = new Fann("/Users/prokop06/IdeaProjects/F.R.I.D.A.Y/src/main/java/com/by/vk_bot/ann");
         float[][] tests = {
                 {1.0f, 0, 1},
                 {0.9f, 1, 3},
                 {0.3f, 0, 8},
-                {1, 1, 8},
+                {1.0f, 1, 8},
                 {0.1f, 0, 0},
         };
         for (float[] test:tests){
@@ -29,7 +29,7 @@ public class LaunchNetwork {
 
     private static String getAction(float[] out){
         int i = 0;
-        for (int j = 1; j < 4; j++) {
+        for (int j = 0; j < 4; j++) {
             if(out[i]<out[j]){
                 i = j;
             }
@@ -57,7 +57,8 @@ public class LaunchNetwork {
         /* Проведем обучение взяв уроки из файла, с максимальным колличеством
            циклов 100000, показывая отчет каждую 100ю итерацию и добиваемся
         ошибки меньше 0.0001 */
-        trainer.train(path, 100000, 100, 0.0001f);
-        fann.save("ann");
+        trainer.train(path, 1000000, 100, 0.00001f);
+        fann.save("/Users/prokop06/IdeaProjects/F.R.I.D.A.Y/src/main/java/com/by/vk_bot/ann");
+
     }
 }
